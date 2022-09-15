34ee94c19bddf004a15ba9771aa1793bf1ca1520
CS <happyclown512@gmail.com>
2022-09-15 11:18:21 -0500

    done

---
+++ EnterToWin/CongratulationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CongratulationsViewController.swift
+//  EnterToWin
+//
+//  Created by Caden on 9/14/22.
+//
+
+import UIKit
+
+class CongratulationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- EnterToWin/ViewController.swift
+++ EnterToWin/ViewController.swift
@@ -9,6 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EmailTF: UITextField!
+    
+    @IBAction func SubmitButton(_ sender: Any) {
+        print(EmailTF.text!)
+        if(EmailTF.text!.isEmpty){
+            print("empty")
+            performLabTextFieldShakeAnimation()
+        } else {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "congratulations") as! CongratulationsViewController
+            self.present(nextViewController, animated:true, completion:nil)
+        }
+    }
+    private func performLabTextFieldShakeAnimation(){
+        UIView.animate(withDuration: 0.1, animations: {
+            let rightTransform = CGAffineTransform(translationX: 20, y: 0)
+            self.EmailTF.transform = rightTransform
+        }) { (_) in
+            UIView.animate(withDuration: 0.1, animations: {
+                self.EmailTF.transform = CGAffineTransform.identity
+            })
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
